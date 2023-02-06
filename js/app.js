3e4ceac92d1c4ea962e19db8f6ffdfbce161a65b
GiuseppeCerto <116110501+GiuseppeCerto@users.noreply.github.com>
2023-02-06 18:27:03 +0100

    EDIT carousel + try bonus

--- js/app.js
+++ js/app.js
@@ -3,17 +3,61 @@
 
 const slideElements = document.getElementsByClassName('slide')
 
-console.log(slideElements)
-
 let indexActiveSlide = 0
 
-const rightBtnElement = document.querySelector('.carousel-arrow.arrow-right')
+// Riproduco il markup delle slides con js
 
-console.log(rightBtnElement)
+
+const slideImages= [
+    './img/img-1.jpeg',
+    './img/img-2.jpeg',
+    './img/img-3.jpeg',
+    './img/img-4.jpeg',
+    './img/img-5.jpg',
+    './img/img-6.jpg'
+]
+
+
+const carouselSection = document.getElementById('carousel-section')
+
+
+carouselSection.innerHTML += (
+
+`
+<div class="slide active">
+    <img src="${slideImages[0]}" alt="">
+</div> 
+        
+<div class="slide">
+    <img src="${slideImages[1]}" alt="">
+</div>
+        
+<div class="slide">
+    <img src="${slideImages[2]}" alt="">
+</div>
+        
+<div class="slide">
+    <img src="${slideImages[3]}" alt="">
+</div>
+
+<div class="slide">
+    <img src="${slideImages[4]}" alt="">
+</div>
+
+<div class="slide">
+    <img src="${slideImages[5]}" alt="">
+</div>
+`
+)
+
+const rightBtnElement = document.getElementById('btnright')
+
 
 // Creare un carolsello con 6 img
 
 rightBtnElement.addEventListener('click', function () {
+
+    console.log("Bottone destro")
 
 	let slideCorrente = slideElements[indexActiveSlide]
 
@@ -25,18 +69,25 @@ rightBtnElement.addEventListener('click', function () {
 
 	prossimaSlide.classList.add('active')
 
-	console.log('next slide', indexActiveSlide)
+    if(indexActiveSlide >= 5){
+
+        slideElements[0]
+
+        prossimaSlide.classList.add('active')
+
+    }
 
 })
 
 
-const leftBtnElement = document.querySelector('.carousel-arrow.arrow-left')
+const leftBtnElement = document.getElementById('btnleft')
 
-console.log(leftBtnElement)
 
 // Creo il carosello con bottone left per il riavvolgimento delle slides
 
 leftBtnElement.addEventListener('click', function () {
+
+    console.log("Bottone sinistro")
 
 	let slideCorrente = slideElements[indexActiveSlide]
 
@@ -49,60 +100,8 @@ leftBtnElement.addEventListener('click', function () {
 	precedenteSlide.classList.add('active')
 
 	console.log('next slide', indexActiveSlide)
+
 })
 
-// Riproduco il markup delle slides con js
-
-
-const slideImages= [
-    './img/img-1.jpeg',
-    './img/img-2.jpeg',
-    './img/img-3.jpeg',
-    './img/img-4.jpeg',
-    './img/img-5.jpeg',
-    './img/img-6.jpeg'
-]
-
-console.log(slideImages)
-
-const carouselSection = document.getElementById('carousel-section')
-
-console.log(carouselSection.innerHTML)
-
-carouselSection.innerHTML = (
-
-`<div class="slide active">
-    <img src="${slideImages[0]}" alt="">
-</div> 
-        
-<div class="slide">
-    <img src="${slideImages[0]}" alt="">
-</div>
-        
-<div class="slide">
-    <img src="${slideImages[0]}" alt="">
-</div>
-        
-<div class="slide">
-    <img src="${slideImages[0]}" alt="">
-</div>
-
-<div class="slide">
-    <img src="${slideImages[0]}" alt="">
-</div>
-
-<div class="slide">
-    <img src="${slideImages[0]}" alt="">
-</div>
-
-<div class="carousel-arrow arrow-left">
-    <i class="fa-solid fa-chevron-left"></i>
-</div>
-
-<div class="carousel-arrow arrow-right">
-    <i class="fa-solid fa-chevron-right"></i>
-</div>`
-)
-
-console.log(carouselSection.innerHTML)
+// console.log(carouselSection.innerHTML)
 
